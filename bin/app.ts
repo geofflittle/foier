d3fafdea5c846f8066d395544a3c759882fd83ce
Geoff Little <g.little712@gmail.com>
2020-07-01 17:53:28 -0500

    Working stack

---
+++ bin/app.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import { App } from "@aws-cdk/core"
+import { FoierStack } from "../lib/foier-stack"
+
+const app = new App();
+new FoierStack(app, "Foier")
---
+++ lib/foier-stack.ts
@@ -0,0 +1,7 @@
+import { App, Construct, Stack } from "@aws-cdk/core"
+
+export class FoierStack extends Stack {
+    constructor(scope: Construct, id: string) {
+        super(scope, id)
+    }
+}
